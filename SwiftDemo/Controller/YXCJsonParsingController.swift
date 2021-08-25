da83f94f710a4033de3578aa8db222e35ec3d4f4
guoguangtao <Jude_guo@163.com>
2021-08-25 17:35:03 +0800

    Json 原生解析

--- SwiftDemo/Controller/YXCJsonParsingController.swift
+++ SwiftDemo/Controller/YXCJsonParsingController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-
+// https://juejin.cn/post/6971997599725256734
 /// 使用原生解析 Json 数据，并且模型化
 class YXCJsonParsingController: UIViewController {
     
-    let label = UILabel()
+    let label = UILabel().then {
+        $0.numberOfLines = 0
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +23,7 @@ class YXCJsonParsingController: UIViewController {
         
         let json = #"""
             {
+                "id" : "1",
                 "name" : "Bob",
                 "age" : 20
             }
@@ -30,23 +33,36 @@ class YXCJsonParsingController: UIViewController {
         
         self.view.addSubview(label)
         label.snp.makeConstraints {
-            $0.center.equalToSuperview()
+            $0.left.equalToSuperview().offset(20)
+            $0.right.equalToSuperview().offset(-20)
+            $0.centerY.equalToSuperview()
         }
         
         label.text = person?.description
+        
+        print(person ?? "Person = nil")
     }
 }
 
 struct YXCPerson: Codable, CustomDebugStringConvertible, CustomStringConvertible {
     
+    let studentId: String?
+    
     /// 名称
     let name: String?
     
     /// 年龄
-    let age: Int?
+    let age: String?
+    
+    /// 跟后台返回的键值不一致配置
+    enum CodingKeys: String, CodingKey {
+        case studentId = "id"
+        case name
+        case age
+    }
     
     var debugDescription: String {
-        "Person.name : \(name ?? "nil"), Person.age : \(age ?? 0)"
+        "Person"
     }
     
     var description: String {
