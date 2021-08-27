3e4f50c4f6e063d9ae5ab857b1b6c092b749a134
guoguangtao <Jude_guo@163.com>
2021-08-27 16:52:26 +0800

    请求网络数据并展示

--- SwiftDemo/Controller/YXCSwityJsonController.swift
+++ SwiftDemo/Controller/YXCSwityJsonController.swift
@@ -14,16 +14,36 @@ class YXCSwityJsonController: UIViewController {
     typealias YXCCompletion = (_ result: Result<YXCThirdModel, Error>) -> Void
     
     let provider = MoyaProvider<HSECLiveShop>()
+    var dataModel: YXCThirdModel?
+    
+    let tableView = UITableView().then {
+        $0.tableFooterView = UIView()
+        $0.rowHeight = 50
+        UITableViewCell.yxc_registerCell($0)
+    }
+    
+    deinit {
+        print("YXCSwityJsonController 被释放")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .systemBackground
         
+        self.view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
         requestDataByNative { result in
             switch result {
             case let .success(dict):
-                print(dict)
+                self.dataModel = dict
+                self.tableView.reloadData()
             case let .failure(error):
                 print(error)
             }
@@ -66,6 +86,56 @@ class YXCSwityJsonController: UIViewController {
             completion(.failure(error))
         }
 
+    }
+}
+
+extension YXCSwityJsonController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return (dataModel?.HSXT_APP_COMMUNITY_ID?.count) ?? 0
+        case 1:
+            return (dataModel?.HSXT_APP_EC_ID?.count) ?? 0
+        case 2:
+            return (dataModel?.HSXT_APP_HOME_ID?.count) ?? 0
+        case 3:
+            return (dataModel?.HSXT_APP_MINE_ID?.count) ?? 0
+        case 4:
+            return (dataModel?.HSXT_APP_MSG_ID?.count) ?? 0
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.yxc_dequeueReusableCell(forTableView: tableView, atIndexPath: indexPath)
+        var string: String?
+        switch indexPath.section {
+        case 0:
+            string = dataModel?.HSXT_APP_COMMUNITY_ID?[indexPath.row].categoryName
+        case 1:
+            string = dataModel?.HSXT_APP_EC_ID?[indexPath.row].categoryName
+        case 2:
+            string = dataModel?.HSXT_APP_HOME_ID?[indexPath.row].categoryName
+        case 3:
+            string = dataModel?.HSXT_APP_MINE_ID?[indexPath.row].categoryName
+        case 4:
+            string = dataModel?.HSXT_APP_MSG_ID?[indexPath.row].categoryName
+        default:
+            string = "nil"
+        }
+        cell.textLabel?.text = string
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
